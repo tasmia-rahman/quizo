115a74da42003824bdd78fddd0674c2b50565a31
tasmia-rahman-maliha <maliha1288@gmail.com>
2022-10-11 13:15:49 +0600

    header updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,19 +5,13 @@ import './Header.css';
 const Header = () => {
     return (
         <div className='header'>
-            <nav className='flex'>
-                <div>
-                    <h1>Quizo</h1>
-                </div>
-                <div>
-                    <Link to='/'>Home</Link>
-                    <Link to='/statistics'>Statistics</Link>
-                    <Link to='/blog'>Blog</Link>
-                </div>
-            </nav>
-            <div className='banner'>
-                <img src="https://img.freepik.com/premium-vector/quiz-text-speech-symbols-concept_149152-641.jpg?w=2000" alt="" />
-                <p>Learn something exciting!</p>
+            <div>
+                <h1>Quizo</h1>
+            </div>
+            <div>
+                <Link to='/'>Home</Link>
+                <Link to='/statistics'>Statistics</Link>
+                <Link to='/blog'>Blog</Link>
             </div>
         </div>
     );
