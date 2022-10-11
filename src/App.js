2c240d83476a4454c1b838b9b87875195bb841a5
tasmia-rahman-maliha <maliha1288@gmail.com>
2022-10-11 13:33:56 +0600

    routes definition updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
-import Header from './components/Header/Header';
 import Statistics from './components/Statistics/Statistics';
 import Blog from './components/Blog/Blog';
+import Main from './layout/Main';
+import Home from './components/Home/Home';
 
 function App() {
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <Header></Header>
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: '/home',
+          element: <Home></Home>
+        },
+        {
+          path: '/statistics',
+          element: <Statistics></Statistics>
+        },
+        {
+          path: '/blog',
+          element: <Blog></Blog>
+        }
+      ]
     },
     {
-      path: '/statistics',
-      element: <Statistics></Statistics>
-    },
-    {
-      path: '/blog',
-      element: <Blog></Blog>
+      path: '*',
+      element: <div>404 not found!</div>
     }
   ])
   return (
-    <div>
+    <div className='App'>
       <RouterProvider router={router}></RouterProvider>
     </div>
   );
---
+++ src/components/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div>
+            <div className='banner'>
+                <img src="https://img.freepik.com/premium-vector/quiz-text-speech-symbols-concept_149152-641.jpg?w=2000" alt="" />
+                <p>Learn something exciting!</p>
+            </div>
+            <div className='topics'>
+
+            </div>
+        </div>
+    );
+};
+
+export default Home;
