78d65cac2d72396513efd8b7d9981cc100c81ac3
tasmia-rahman-maliha <maliha1288@gmail.com>
2022-10-11 20:49:29 +0600

    chart added

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
         },
         {
           path: '/statistics',
+          loader: async () => fetch('https://openapi.programming-hero.com/api/quiz'),
           element: <Statistics></Statistics>
         },
         {
---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import './Statistics.css';
+
+const Statistics = () => {
+    const { data } = useLoaderData();
+    console.log(data)
+    return (
+        <div className='chart'>
+            <LineChart
+                width={1000}
+                height={500}
+                data={data}
+                margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                }}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="total" stroke="#8884d8" activeDot={{ r: 8 }} />
+            </LineChart>
+        </div>
+    );
+};
+
+export default Statistics;
